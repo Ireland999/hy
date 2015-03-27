298253021fb37d8fc6152a59aeac9bf0600544fa
wushuang <519969840@qq.com>
2015-03-27 18:15:02 +0800

    0327

--- app/router/app.urgent.js
+++ app/router/app.urgent.js
@@ -1,9 +1,12 @@
 (function () {
   function config($stateProvider,$urlRouterProvider){
     $urlRouterProvider.otherwise("/");
-    $stateProvider.state('center',{
+    // $stateProvider.state('center',{
+    //   url:'/',
+    //   templateUrl:'views/compedetail.html'
+      $stateProvider.state('center',{
       url:'/',
-      templateUrl:'views/visit/greet.html'
+      templateUrl:'views/index.html'
     }).state('center.Competitor',{
       url:'Competitor',
        templateUrl:'views/Competitor.html'
@@ -22,9 +25,12 @@
     }).state('center.greet',{                     //打招呼获取附近终端
       url:'greet',
       templateUrl:'views/visit/greet.html'
-    }).state('center.rebackcode.html',{            //扫一扫反馈信息                
-      url:'rebackcode',
-      templateUrl:'views/rebackcode.html'
+    }).state('center.visit-workorder',{                     //拜访工单（还未完成）
+      url:'visit-workorder',
+      templateUrl:'views/visit/visit-workorder.html'  
+    }).state('center.compedetail',{            //扫一扫反馈信息                
+      url:'compedetail',
+      templateUrl:'views/compedetail.html'
     }).state('center.weijia',{                     //维价
       url:'weijia',
       templateUrl:'views/weijia.html'
